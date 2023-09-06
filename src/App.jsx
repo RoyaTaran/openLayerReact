709d0d155ae1866618bd31009abfd700fc9bb727
RoyaTaran <ro.taran8673@gmail.com>
2023-09-06 23:08:16 +0330

    edit project

--- src/App.jsx
+++ src/App.jsx
@@ -13,21 +13,13 @@ import "./App.css";
 import "ol/ol.css";
 
 function App() {
-  const [map, setMap] = useState();
   const mapElement = useRef();
-  const popupElement = useRef();
-
-  const overlay = new Overlay({
-    element: popupElement.current,
-  });
-
   const osmLayer = new TileLayer({
     preload: Infinity,
     source: new OSM(),
   });
 
   const vectorSource = new VectorSource({});
-
   const vectorLayer = new VectorLayer({
     source: vectorSource,
     style: new Style({
@@ -39,14 +31,12 @@ function App() {
       }),
     }),
   });
-
   useEffect(function () {
     return function () {
       const initialMap = new Map({
         controls: defaultControls({ attribution: false, zoom: false }),
         target: mapElement.current,
         layers: [osmLayer, vectorLayer],
-        overlays: [overlay],
         view: new View({
           projection: "EPSG:4326",
           center: [48.49597353548832, 36.67851410709572],
@@ -58,26 +48,12 @@ function App() {
         type: "Point",
       });
       initialMap.addInteraction(draw);
-      setMap(initialMap);
     };
   }, []);
 
   return (
     <div className="container">
-      <div>
-        {" "}
-        <div ref={mapElement} className="map-container" />
-        <div ref={popupElement} id="popup">
-          <div id="popup-content"></div>
-        </div>
-      </div>
-
-      <div dir="rtl" className="input-container">
-        <input id="title" type="text" placeholder="عنوان" />
-        <input id="description" type="text" placeholder="توضیحات" />
-        <input id="entry" type="button" value="ثبت" />
-        <input id="show" type="button" value="نمایش" />
-      </div>
+      <div ref={mapElement} className="map-container" />
     </div>
   );
 }
